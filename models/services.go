f2c7dc21f4ab63abde4bad65e8655efa614264e2
Naveen Kunareddy <gosmartwizard@gmail.com>
2022-08-02 10:39:02 +0530

    Moved the close and migrate functions to the services type

--- models/services.go
+++ models/services.go
@@ -2,6 +2,12 @@ package models
 
 import "github.com/jinzhu/gorm"
 
+type Services struct {
+	Gallery GalleryService
+	User    UserService
+	db      *gorm.DB
+}
+
 func NewServices(connectionInfo string) (*Services, error) {
 	db, err := gorm.Open("postgres", connectionInfo)
 	if err != nil {
@@ -10,10 +16,22 @@ func NewServices(connectionInfo string) (*Services, error) {
 	db.LogMode(true)
 	return &Services{
 		User: NewUserService(db),
+		db:   db,
 	}, nil
 }
 
-type Services struct {
-	Gallery GalleryService
-	User    UserService
+func (s *Services) Close() error {
+	return s.db.Close()
+}
+
+func (s *Services) DestructiveReset() error {
+	err := s.db.DropTableIfExists(&User{}, &Gallery{}).Error
+	if err != nil {
+		return err
+	}
+	return s.AutoMigrate()
+}
+
+func (s *Services) AutoMigrate() error {
+	return s.db.AutoMigrate(&User{}, &Gallery{}).Error
 }
